ab976ba3b173ecc57e2f404003993985cd173a5c
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-10-24 13:17:21 -0200

    tela de perdeu/venceu

--- js/script.js
+++ js/script.js
@@ -8,9 +8,9 @@ var comecou = false;
 var letrasChutadas = [];
 var palavra = "";
 
-$("#cadastro button").click(function(event) { //("#btnCadastrar") você pode chamar como chama no css, por exemplo ("#cadastro button")
+$("#cadastro button").click(function (event) { //("#btnCadastrar") você pode chamar como chama no css, por exemplo ("#cadastro button")
     event.preventDefault();
-    
+
     //Declarar variável palavra com o valor do input
     palavra = $("#palavra").val();
 
@@ -22,65 +22,72 @@ $("#cadastro button").click(function(event) { //("#btnCadastrar") você pode cha
         //Se não estiver vazio, montar os underlines
         palavra = palavra.trim();
 
-        for (i=0; i < palavra.length; i++) {
-            var span = $("<span>"+palavra[i]+"</span>");
+        for (i = 0; i < palavra.length; i++) {
+            var span = $("<span>" + palavra[i] + "</span>");
             span.appendTo(".letras");
             console.log(palavra);
         }
-        
+
         //Se não estiver vazio mostrar a tela forca
-            //Adicionar classe na tela da forca
-            $("#forca").addClass("visivel");
-            //Remover classe na tela do cadastro
-            $("#cadastro").removeClass("visivel");
-            comecou = true;
+        //Adicionar classe na tela da forca
+        $("#forca").addClass("visivel");
+        //Remover classe na tela do cadastro
+        $("#cadastro").removeClass("visivel");
+        comecou = true;
     }
-    
+
 });
 
-$(document).keydown(function(event) {
+var erro = 0;
+var existe = false;
+$(document).keydown(function (event) {
     if (comecou) {
+
         var letra = event.key;
         //Validando se é apenas uma letra
         if (letra.length > 1) {
             return;
         }
         //Vai verificar se a letra já foi utilizada
-        if(letrasChutadas.indexOf(letra) != -1) {
+        if (letrasChutadas.indexOf(letra) != -1) {
             return;
         }
 
         //Registrar a letra utilizada
         letrasChutadas.push(letra);
-        var span = $("<span>"+letra+"<span>");
+        var span = $("<span>" + letra + "<span>");
         span.appendTo(".letras-usadas");
-        //Letra existe na palavra cadastrada?
-        if(letrasChutadas.indexOf(letra) != -1) {
-            //Se sim 
-                //mostra no campo a letra correspondente
-                for (var i=0 ; i<palavra.length; i++) {
-                    var letra2 = palavra[i];
-                    if (letra == letra2) {                  
-                        $(".letras span").eq(i).addClass("visivel"); //eq é pra pegar o índice
-                    } 
-                }
-                //Se a palavra estiver completa
-                if($(".letras span:not(.visivel)").length == 0) {
-                    //Mostra o final correto
-                    $("#ganhou").addClass("visivel");
-                    $("#perdeu").removeClass("visivel");
-                }
-                    
-        } else {
-            //Se não 
-                //mostra o membro do boneco
-                //Se excedeu as tentativas
-                    //Mostra a família triste
-        }
-            
-
-            
         
+        //Letra existe na palavra cadastrada?
+        if (palavra.indexOf(letra) != -1) {
+            //Se sim 
+            //mostra no campo a letra correspondente
+            for (var i = 0; i < palavra.length; i++) {
+                var letra2 = palavra[i];
+                if (letra == letra2) {
+                    $(".letras span").eq(i).addClass("visivel"); //eq é pra pegar o índice
+                    existe = true;
+                } else {
+                    existe = false;
+                }
+            }
+            //Se a palavra estiver completa
+            if ($(".letras span:not(.visivel)").length == 0) {
+                //Mostra o final correto
+                $("#ganhou").addClass("visivel");
+                $("#forca").removeClass("visivel");
+            }
+
+        } else {
+            //Se não  
+            //TA ERRADOOOOOOO
+            erro++;
+            $(".corpo .st0").eq(erro-1).attr("class","st0 visivel");
+            if(erro > 5){
+                $("#forca").removeClass("visivel");
+                $("#perdeu").addClass("visivel");
+            }
+        }
     }
 
 });
